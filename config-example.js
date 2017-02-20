0bc3bb18ad1a120129a6fbd3d98f376ebbf29727
charlesbrandontheis <charlesbrandontheis@gmail.com>
2017-02-20 09:46:00 -0700

    Update config-example.js

--- config-example.js
+++ config-example.js
@@ -2,11 +2,11 @@ var slackbot = require('./lib/bot');
 
 var config = {
     bot_name: "WebexBot",//Provide the name to post under.  Default is WebexBo
-    token: 'XXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXX',
+    token: 'xoxp-24058771809-24057187122-143896304578-88da39bdc138385910a4c25aca01713a',
     webex_urls: {
-      "launch": "https://mypath.webex.com/meet/",//REPLACE THESE W/ YOUR COMPANY URLs
-      "join": "https://mypath.webex.com/join/",
-      "DEFAULT": "https://mypath.webex.com/"
+      "launch": "https://ibm2.webex.com/meet/cbtheis",//REPLACE THESE W/ YOUR COMPANY URLs
+      "join": "https://ibm2.webex.com/join/cbtheis",
+      "DEFAULT": "https://ibm2.webex.com/"
     },
     pattern: /^\:webex\s+(personal\s+(\w+))\s*$/, //Default is: /webex personal username
     verbose: true,
